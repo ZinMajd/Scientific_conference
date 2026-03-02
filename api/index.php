40fc0142dd02555a80fccacda63653ffd338012d
ZinMajd <https://github.com/ZinMajd/Scientific_conferences.git>
2026-03-02 05:35:18 +0300

    fix(vercel): stop Laravel from stripping API prefix

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
+// Fix for Vercel: Prevent Laravel from stripping the '/api' prefix
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['PHP_SELF'] = '/index.php';
+
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
