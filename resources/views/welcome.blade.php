e4d6fed66068d827bcfe90d3a8caaaf2aace0e5e
ZinMajd <https://github.com/ZinMajd/Scientific_conferences.git>
2026-03-03 06:34:21 +0300

    Fix: Set locale to Arabic and disable auto-translation to prevent UI glitches in Chrome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
+<html lang="ar" dir="rtl">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google" content="notranslate">
     <title>Scientific Conference Management</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
