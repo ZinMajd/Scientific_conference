b7d3ee9ef766c92bd69ba3dafa80efcc62eafe03
ZinMajd <https://github.com/ZinMajd/Scientific_conferences.git>
2026-07-06 22:54:22 +0300

    Fix: PostgreSQL boolean datatype mismatch for is_active column in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -162,7 +162,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
-            'is_active' => 'boolean',
+            'is_active' => \App\Casts\PgsqlBoolean::class,
         ];
     }
 }
